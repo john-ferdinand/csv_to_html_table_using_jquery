ecbfeedd1e7f7058d261dfbbf298ed3614777fe2
Jordan <ferdinandantonio75@gmail.com>
2018-07-08 10:01:41 +0800

    adding all files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Displaying CSV data to HTML Table using JQuery</title>
+</head>
+
+<body>
+	<input type="file" name="pic" accept=".csv">
+</body>
+
+</html>
